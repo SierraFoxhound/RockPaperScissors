10be85669c90653f5c62f98248d9f201ec91079e
dsoto <ds580999@gmail.com>
2023-05-26 17:27:48 -0700

    add round function

--- javascript.js
+++ javascript.js
@@ -1,20 +1,53 @@
 
 //Create function getComputerChoice, will return rock, etc
-var arr = ['Rock', 'Paper', 'Scissors']
+const rock = 'Rock'
+const paper = 'Paper'
+const scissors = 'Scissors'
+var arr = [rock, paper, scissors]
 var idx = Math.floor(Math.random() * arr.length);
 
-function getComputerChoice () {
-   return (arr[idx]);
+function getComputerChoice (arr, idx) {
+   let choice = (arr[idx]);
+   return choice
 }
 
 // Use console.log() to get result
-console.log(arr[idx])
+console.log(getComputerChoice(arr,idx));
 
 /*
 Write function that plays 1 round, have two parameters 
  (playerSelection, computerSelection) then return string showing 
  winner. Side note: make user input case-insensitve
 */
+function match(playerSelection, computerSelection) {
+   if (playerSelection === 'rock', computerSelection === rock) {
+      para.textContent = 'Tie'
+   }
+   else if (playerSelection === 'rock', computerSelection === paper) {
+      para.textContent = 'Win'
+   }
+   else if (playerSelection === 'rock', computerSelection === scissors) {
+      para.textContent = 'Lose'
+   } else if (playerSelection === 'paper', computerSelection === rock) {
+      para.textContent = 'Win'
+   }
+   else if (playerSelection === 'paper', computerSelection === paper) {
+      para.textContent = 'Tie'
+   }
+   else if (playerSelection === 'paper', computerSelection === scissors) {
+      para.textContent = 'Lose'
+   } else if (playerSelection === 'scissors', computerSelection === rock) {
+      para.textContent = 'Lose'
+   }
+   else if (playerSelection === 'scissors', computerSelection === paper) {
+      para.textContent = 'Win'
+   }
+   else if (playerSelection === 'scissors', computerSelection === scissors) {
+      para.textContent = 'Tie'
+   }
+
+}
+
 
 // return results of last function, going to need results for later
 //console.log results to see if it works
